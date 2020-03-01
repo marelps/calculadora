d5b76ca6208e28bc0fdb5dc309ecff0d7c87e87a
Maria Vitória Garrucho Campos <vitoriagarrucho@gmail.com>
2020-02-29 21:21:38 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>
+			Formulário
+		</title>
+	</head>
+	<body>
+        <form method="post" action="pag2.php">
+<select name="OP">
+
+<option value="subtracao">Subtrair</option>
+<option value="divisao">Dividir</option>
+<option value="adicao">Somar</option>
+<option value="multiplicacao">Multiplicar</option>
+
+</select>
+<br />
+<br />
+1: <input type="text" 		name="N1"		/>	<br>
+2: <input type="text" 		name="N2"		/>	<br>
+<input type="submit" value="Calcular" />
+ </form>
+</body>
+</html>
---
+++ pag2.php
@@ -0,0 +1,27 @@
+<?php
+$_POST['N1'];
+$_POST['N2'];
+$_POST['OP'];
+$r = 0;	
+	switch($_POST['OP']){
+		case "multiplicacao":
+		
+		$r = $_POST['N1']*$_POST['N2'];
+		echo $r;
+		break;
+		case "divisao":
+		$r = $_POST['N1']/$_POST['N2'];
+			echo $r;
+		break;
+		case "adicao":
+		$r = $_POST['N1']+$_POST['N2'];
+		echo $r;
+		break;
+		case "subtracao":
+		$r = $_POST['N1']-$_POST['N2'];
+			echo $r;
+		break;
+		default:
+echo "Digite apenas números e uma operação";
+}
+?>
